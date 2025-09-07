c0f3a68152b110d41e00d09cfabaeb2efee394c4
Leap <git@leap.new>
2025-09-07 14:22:53 +0000

    GitHub: wwwwwwwwwwwwwww

--- frontend/pages/KanbanPage.tsx
+++ frontend/pages/KanbanPage.tsx
@@ -85,8 +85,8 @@ export default function KanbanPage() {
             Dashboard
           </Button>
         </div>
-        <div className="flex-1 overflow-hidden">
-          <div className="h-full overflow-x-auto overflow-y-hidden">
+        <div className="flex-1 min-h-0">
+          <div className="h-full overflow-auto">
             <KanbanBoard 
               phases={developmentPhases} 
               allFeatures={allFeatures} 
